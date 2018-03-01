7eb0af8a78f697a0d0f38c9db892cbaaaafaaf2c
jsmichot <jsmichot@gmail.com>
2018-03-01 16:45:54 -0600

    (scaffold) add server directory

---
+++ server/helpers.js
@@ -0,0 +1,2 @@
+const request = require('request');
+const dotenv = require('dotenv').config();
---
+++ server/routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const helper = require('./helpers');
+
+router.get('/', (req, res) => {});
+
+module.exports = router;
