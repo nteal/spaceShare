85403ee357c12c023a9e8da1cd0c208b24076f21
jsmichot <jsmichot@gmail.com>
2018-03-02 10:50:52 -0600

    (scaffold) adds CreateSpace component and views

---
+++ client/src/components/create-a-space/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class CreateSpace extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {/* create a space */}
+      </div>
+    );
+  }
+}
+
+export default CreateSpace;
