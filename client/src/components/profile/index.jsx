272d6b6328602d2a2ded44c74df2ae0c7e3d881a
jsmichot <jsmichot@gmail.com>
2018-03-02 11:08:51 -0600

    (scaffold) adds profile component and view

---
+++ client/src/components/profile/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Profile extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {/* profile */}
+      </div>
+    );
+  }
+}
+
+export default Profile;
