e3835cc8f44b314abb005b55aaba1c2a556fe799
jsmichot <jsmichot@gmail.com>
2018-03-02 11:11:49 -0600

    (scaffold) adds SearchResults component and views

---
+++ client/src/components/search-results/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class SearchResults extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {/* search results */}
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
