b2b5a8aab0d64dde17f319fbf5c35d26ef5aee4a
jsmichot <jsmichot@gmail.com>
2018-03-06 13:29:13 -0600

    (init) adds row for common-area's main_image

--- client/src/components/common-area/index.jsx
+++ client/src/components/common-area/index.jsx
@@ -15,6 +15,9 @@ class CommonArea extends React.Component {
     return (
       <div>
         <h1>Common Area</h1>
+        <div className="row">
+          <img src="https://cdn.vox-cdn.com/thumbor/1XgXyrtwx2tg7FK9gVZfomt7B54=/0x600/cdn.vox-cdn.com/uploads/chorus_asset/file/4608573/Screen_20Shot_202015-03-23_20at_201.14.01_20PM.0.png" alt="https://kaggle2.blob.core.windows.net/competitions/kaggle/5407/media/housesbanner.png" />
+        </div>
         <div>
           <Todos />
         </div>
