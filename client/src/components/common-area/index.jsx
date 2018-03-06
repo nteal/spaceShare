284a98d7e83abfa1efd6e1e87aedb1701782fcc2
jsmichot <jsmichot@gmail.com>
2018-03-06 14:32:52 -0600

    (init) sets divisions for grid design

--- client/src/components/common-area/index.jsx
+++ client/src/components/common-area/index.jsx
@@ -14,14 +14,37 @@ class CommonArea extends React.Component {
   render() {
     return (
       <div>
-        <h1>Common Area</h1>
-        <div className="row">
+
+        <div className="row-image">
           <img src="https://cdn.vox-cdn.com/thumbor/1XgXyrtwx2tg7FK9gVZfomt7B54=/0x600/cdn.vox-cdn.com/uploads/chorus_asset/file/4608573/Screen_20Shot_202015-03-23_20at_201.14.01_20PM.0.png" alt="https://kaggle2.blob.core.windows.net/competitions/kaggle/5407/media/housesbanner.png" />
         </div>
-        <div>
-          <Todos />
+
+        <div className="row-buttons">
+          <div className="col-info">
+            <div className="row-info">
+              <h1>Common Area</h1>
+              <button>info</button>
+            </div>
+          </div>
+          <div className="col-members-rules">
+            <div className="row-members">
+              <button>Our Members</button>
+            </div>
+            <div className="row-rules">
+              <button>Our Ground Rules</button>
+            </div>
+          </div>
         </div>
-        {/* chat */}
+
+        <div className="row-todos-chat">
+          <div className="col-todos">
+            <Todos />
+          </div>
+          <div className="col-chat">
+            {/* chat */}
+          </div>
+        </div>
+
       </div>
     );
   }
