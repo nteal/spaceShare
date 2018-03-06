78de76f1d9ff01d432657a65cdf40713e9c1ce55
Nicholas Teal <nteal15@gmail.com>
2018-03-06 16:05:15 -0600

    (init) requries db in server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
+const db = require('../database');
 
 const app = express();
 const pathway = path.join(__dirname, '/../client/dist');
