dd10f8afa51bcc45435d10767ece2cfbe98a96e8
Emily Yu <emilyyu518@gmail.com>
2018-03-05 22:10:28 -0600

    (init) sets up side nav

--- client/src/components/nav/index.jsx
+++ client/src/components/nav/index.jsx
@@ -1,18 +1,97 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Link, Switch, Route } from 'react-router-dom';
 import Axios from 'axios';
-import TopNav from './top-nav.jsx';
+import { withStyles } from 'material-ui/styles';
+import classNames from 'classnames';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import List from 'material-ui/List';
+import Typography from 'material-ui/Typography';
+import Divider from 'material-ui/Divider';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import ChevronLeftIcon from 'material-ui-icons/ChevronLeft';
+import ChevronRightIcon from 'material-ui-icons/ChevronRight';
+
 import Dashboard from '../dashboard/index.jsx';
+import ChatMain from '../chat-main/index.jsx';
 import CommonArea from '../common-area/index.jsx';
 import CreateSpace from '../create-a-space/index.jsx';
 import Search from '../search/index.jsx';
 import SearchResults from '../search-results/index.jsx';
+import SideNavListItems from './side-nav.jsx';
 
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 430,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    width: 60,
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    // ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
 class Nav extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      drawerOpen: false,
     };
+    this.handleDrawerOpen = this.handleDrawerOpen.bind(this);
+    this.handleDrawerClose = this.handleDrawerClose.bind(this);
   }
   componentDidMount() {
     // check if user is authenticated; if they aren't, redirect to /
@@ -27,24 +106,67 @@ class Nav extends React.Component {
       });
   }
 
+  handleDrawerOpen() {
+    this.setState({ drawerOpen: true });
+  }
+
+  handleDrawerClose() {
+    this.setState({ drawerOpen: false });
+  }
+
+
   render() {
+    const { classes, theme } = this.props;
+
     return (
-      <div>
-        {/* top nav */}
-        <TopNav />
-        {/* side nav */}
-
-
-        <Switch>
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/common-area" component={CommonArea} />
-          <Route path="/new-space" component={CreateSpace} />
-          <Route path="/search" component={Search} />
-          <Route path="/listings" component={SearchResults} />
-        </Switch>
+      <div className={classes.root}>
+        <AppBar
+          position="absolute"
+          className={classNames(classes.appBar, this.state.drawerOpen && classes.appBarShift)}
+        >
+          <Toolbar disableGutters={!this.state.open}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerOpen}
+              className={classNames(classes.menuButton, this.state.drawerOpen && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography component={Link} to="/" variant="title" color="inherit" noWrap>
+              SpaceShare
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classNames(classes.drawerPaper, !this.state.drawerOpen && classes.drawerPaperClose),
+          }}
+          open={this.state.open}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={this.handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>{SideNavListItems}</List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Switch>
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/common-area" component={CommonArea} />
+            <Route path="/messages" component={ChatMain} />
+            <Route path="/new-space" component={CreateSpace} />
+            <Route path="/search" component={Search} />
+            <Route path="/listings" component={SearchResults} />
+          </Switch>
+        </main>
       </div>
     );
   }
 }
 
-export default Nav;
+export default withStyles(styles, { withTheme: true })(Nav);
---
+++ client/src/components/nav/side-nav.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import HomeIcon from 'material-ui-icons/Home';
+import MailIcon from 'material-ui-icons/Mail';
+import AddLocationIcon from 'material-ui-icons/AddLocation';
+import SearchIcon from 'material-ui-icons/Search';
+import BookIcon from 'material-ui-icons/Book';
+import ListIcon from 'material-ui-icons/List';
+
+const SideNavListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <HomeIcon />
+      </ListItemIcon>
+      <ListItemText primary="Your Spaces" />
+    </ListItem>
+    <ListItem button component={Link} to="/messages">
+      <ListItemIcon>
+        <MailIcon />
+      </ListItemIcon>
+      <ListItemText primary="Messages" />
+    </ListItem>
+    <ListItem button component={Link} to="/new-space">
+      <ListItemIcon>
+        <AddLocationIcon />
+      </ListItemIcon>
+      <ListItemText primary="New Space" />
+    </ListItem>
+    <ListItem button component={Link} to="/search">
+      <ListItemIcon>
+        <SearchIcon />
+      </ListItemIcon>
+      <ListItemText primary="New Search" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <BookIcon />
+      </ListItemIcon>
+      <ListItemText primary="Saved Searches" />
+    </ListItem>
+    <ListItem button component={Link} to="/listings">
+      <ListItemIcon>
+        <ListIcon />
+      </ListItemIcon>
+      <ListItemText primary="All Listings" />
+    </ListItem>
+  </div>
+);
+
+export default SideNavListItems;
