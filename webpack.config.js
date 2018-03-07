64c6c0583225e0bfc22fd4d1e16b20e54ad06c4b
Emily Yu <emilyyu518@gmail.com>
2018-03-06 21:18:30 -0600

    (refactor) sets up webpack to compile stylesheet

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
           presets: ['react', 'es2015'],
         },
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   devServer: {
