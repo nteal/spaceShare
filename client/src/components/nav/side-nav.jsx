eda35f947ad7a8091834717f3e4e012f78472e47
Emily Yu <emilyyu518@gmail.com>
2018-03-06 20:52:29 -0600

    (refactor) refactors nav to react sidebar / bootstrap

--- client/src/components/nav/side-nav.jsx
+++ client/src/components/nav/side-nav.jsx
@@ -7,46 +7,72 @@ import AddLocationIcon from 'material-ui-icons/AddLocation';
 import SearchIcon from 'material-ui-icons/Search';
 import BookIcon from 'material-ui-icons/Book';
 import ListIcon from 'material-ui-icons/List';
+import Header from './header.jsx';
 
-const SideNavListItems = (
-  <div>
-    <ListItem button>
-      <ListItemIcon>
-        <HomeIcon />
-      </ListItemIcon>
-      <ListItemText primary="Your Spaces" />
-    </ListItem>
-    <ListItem button component={Link} to="/messages">
-      <ListItemIcon>
-        <MailIcon />
-      </ListItemIcon>
-      <ListItemText primary="Messages" />
-    </ListItem>
-    <ListItem button component={Link} to="/new-space">
-      <ListItemIcon>
-        <AddLocationIcon />
-      </ListItemIcon>
-      <ListItemText primary="New Space" />
-    </ListItem>
-    <ListItem button component={Link} to="/search">
-      <ListItemIcon>
-        <SearchIcon />
-      </ListItemIcon>
-      <ListItemText primary="New Search" />
-    </ListItem>
-    <ListItem button component={Link} to="/saved-searches">
-      <ListItemIcon>
-        <BookIcon />
-      </ListItemIcon>
-      <ListItemText primary="Saved Searches" />
-    </ListItem>
-    <ListItem button component={Link} to="/listings">
-      <ListItemIcon>
-        <ListIcon />
-      </ListItemIcon>
-      <ListItemText primary="All Listings" />
-    </ListItem>
-  </div>
-);
+const styles = {
+  sidebar: {
+    width: 256,
+    height: '100%',
+  },
+  sidebarLink: {
+    display: 'block',
+    padding: '16px 0px',
+    color: 'white',
+    textDecoration: 'none',
+    backgroundColor: '#6F5BC0',
+  },
+  divider: {
+    margin: '8px 0',
+    height: 1,
+    backgroundColor: 'white',
+  },
+  content: {
+    padding: '16px',
+    height: '100%',
+    backgroundColor: '#6F5BC0',
+  },
+  home: {
+    position: 'relative',
+    top: '1px',
+  },
+};
 
-export default SideNavListItems;
+const SideNavItems = (props) => {
+  const { toggleOpen } = props;
+  const style = styles.sidebar;
+
+  return (
+      <div style={styles.content} onClick={toggleOpen}>
+        <Link className="btn btn-primary" to="/" style={styles.sidebarLink}>
+          <h4>SpaceShare</h4>
+        </Link>
+        <Link className="btn btn-primary" to="/" style={styles.sidebarLink}>
+          <i className="material-icons" style={styles.home}>home</i>
+          Your Spaces
+        </Link>
+        <Link className="btn btn-primary" to="/messages" style={styles.sidebarLink}>
+          <i className="material-icons">mail</i>
+          Messages
+        </Link>
+        <Link className="btn btn-primary" to="/new-space" style={styles.sidebarLink}>
+          <i className="material-icons">add_location</i>
+          New Space
+        </Link>
+        <Link className="btn btn-primary" to="/search" style={styles.sidebarLink}>
+          <i className="material-icons">search</i>
+          New Search
+        </Link>
+        <Link className="btn btn-primary" to="/saved-searches" style={styles.sidebarLink}>
+          <i className="material-icons">book</i>
+          Saved Searches
+        </Link>
+        <Link className="btn btn-primary" to="/listings" style={styles.sidebarLink}>
+          <i className="material-icons">list</i>
+          All Listings
+        </Link>
+      </div>
+  );
+};
+
+
+export default SideNavItems;
