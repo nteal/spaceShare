c947a61d290e395e1755e41951c1f1fe8c9d1fc3
jsmichot <jsmichot@gmail.com>
2018-03-06 18:21:16 -0600

    (init) prep for bootstrap integration

--- client/src/components/common-area/index.jsx
+++ client/src/components/common-area/index.jsx
@@ -15,32 +15,37 @@ class CommonArea extends React.Component {
     return (
       <div>
 
-        <div className="row-image">
-          <img src="https://cdn.vox-cdn.com/thumbor/1XgXyrtwx2tg7FK9gVZfomt7B54=/0x600/cdn.vox-cdn.com/uploads/chorus_asset/file/4608573/Screen_20Shot_202015-03-23_20at_201.14.01_20PM.0.png" alt="https://kaggle2.blob.core.windows.net/competitions/kaggle/5407/media/housesbanner.png" />
+        <div className="row">
+          <div className="media-object">
+            <img
+              src="https://cdn.vox-cdn.com/thumbor/1XgXyrtwx2tg7FK9gVZfomt7B54=/0x600/cdn.vox-cdn.  com/uploads/chorus_asset/file/46 08573/Screen_20Shot_202015-03-23_20at_201.14.01_20PM.0.png"
+              alt="https://kaggle2.blob.core.windows.net/competitions/kaggle/5407/media/housesbanner.png"
+            />
+          </div>
         </div>
 
-        <div className="row-buttons">
-          <div className="col-info">
-            <div className="row-info">
+        <div className="row">
+          <div className="col">
+            <div className="row">
               <h1>Common Area</h1>
               <button>info</button>
             </div>
           </div>
-          <div className="col-members-rules">
-            <div className="row-members">
+          <div className="col">
+            <div className="row">
               <button>Our Members</button>
             </div>
-            <div className="row-rules">
+            <div className="row">
               <button>Our Ground Rules</button>
             </div>
           </div>
         </div>
 
-        <div className="row-todos-chat">
-          <div className="col-todos">
+        <div className="row">
+          <div className="col">
             <Todos />
           </div>
-          <div className="col-chat">
+          <div className="col">
             {/* chat */}
           </div>
         </div>
