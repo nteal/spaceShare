d821ab4f7049502e0d6f95baa274abab107e9587
Nicholas Teal <nteal15@gmail.com>
2018-03-07 12:25:24 -0600

    (test) adds fake data to test user

---
+++ spec/fake_data-db.js
@@ -0,0 +1,18 @@
+const newUser = {
+  about: 'i\'m me! and no one else. except for sometimes',
+  image_url: 'https://i.chzbgr.com/full/9013910528/hAB49129F/',
+  name_first: 'bobo',
+  name_last: 'boberton',
+  phone: '555555555555',
+  email: 'email@email.email',
+  fb_id: 'sadf9pwp8rypf98dsiyr9pfeouyf',
+  fb_link: 'facebook.com',
+  fb_verified: false,
+  searchable_work: false,
+  searchable_live: false,
+  planet: 'Uranus',
+  birthdate: new Date(),
+  gender_id: 0,
+  sleep_id: 1,
+  personalit_id: 1,
+};
