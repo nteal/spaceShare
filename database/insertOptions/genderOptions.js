8f19e76e61c4afee625e5d236e4c88433916ec9f
Nicholas Teal <nteal15@gmail.com>
2018-03-07 19:38:21 -0600

    (fix) populates planets only if empty

--- database/insertOptions/genderOptions.js
+++ database/insertOptions/genderOptions.js
@@ -10,7 +10,7 @@ const addGenders = () => Gender.bulkCreate(initialGenderOptions);
 exports.fillGenderOptions = () => (
   // search for genders
   Gender.findAll()
-    // if already personalities, print them. else add them
+    // if already genders, print them. else add them
     .then(genders => (genders.length ? console.log('genders already populated') : addGenders()))
     .catch(err => console.log(err))
 );
--- database/insertOptions/planetOptions.js
+++ database/insertOptions/planetOptions.js
@@ -1,15 +1,23 @@
 const { Planet } = require('../models/planetModel');
 
+const planetList = [
+  { name: 'Murcery' },
+  { name: 'Venus' },
+  { name: 'Earth' },
+  { name: 'Mars' },
+  { name: 'Jupiter' },
+  { name: 'Saturn' },
+  { name: 'Uranus' },
+  { name: 'Neptune' },
+  { name: 'Pluto' },
+];
+
+const populatePlanets = () => Planet.bulkCreate(planetList);
+
 exports.fillPlanetOptions = () => (
-  Planet.bulkCreate([
-    { name: 'Murcery' },
-    { name: 'Venus' },
-    { name: 'Earth' },
-    { name: 'Mars' },
-    { name: 'Jupiter' },
-    { name: 'Saturn' },
-    { name: 'Uranus' },
-    { name: 'Neptune' },
-    { name: 'Pluto' },
-  ])
+  // search for planets
+  Planet.findAll()
+    // if already planets, print them. else add them
+    .then(planets => (planets.length ? console.log('planets already populated') : populatePlanets()))
+    .catch(err => console.log(err))
 );
