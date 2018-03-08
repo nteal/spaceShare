a7e5bb4815806f42530c419d72ad401b184aa893
Emily Yu <emilyyu518@gmail.com>
2018-03-08 11:52:58 -0600

    (feat) adds back button to ground rules

--- client/src/components/common-area/ground-rules.jsx
+++ client/src/components/common-area/ground-rules.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import ArrowLeftBoldCircle from 'mdi-react/ArrowLeftBoldCircleIcon.js';
 
 const GroundRules = (props) => {
+  const handleBack = () => {
+    props.history.goBack();
+  };
   return (
-    <div>
-      {/* GroundRules */}
+    <div className="container">
+      <div className="row">
+        <div className="heading-box">
+          <h1>Our Ground Rules</h1>
+        </div>
+      </div>
+      <div className="row">
+        <button className="custom-btn" onClick={handleBack}>
+          <ArrowLeftBoldCircle className="mdi-btn" height={50} width={50} fill="#6F5BC0" />
+        </button>
+      </div>
+      <div className="row">
+        {/* ground rules */}
+      </div>
+
     </div>
   );
 };
