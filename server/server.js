fef4c981983e635c597313899c5709a1b668e369
jsmichot <jsmichot@gmail.com>
2018-03-07 22:29:55 -0600

    (feat) adds FacebookTokenStrategy

--- server/server.js
+++ server/server.js
@@ -4,10 +4,21 @@ const path = require('path');
 const passport = require('passport');
 const FacebookTokenStrategy = require('passport-facebook-token');
 const db = require('../database');
+const dotenv = require('dotenv').config();
 
 const app = express();
 const pathway = path.join(__dirname, '/../client/dist');
 app.use(express.static(pathway));
+
+passport.use(new FacebookTokenStrategy(
+  {
+    clientID: process.env.FB_CLIENT_ID,
+    clientSecret: process.env.FB_CLIENT_SECRET,
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // create new user here
+  },
+));
 
 app.use('/', routes);
 
