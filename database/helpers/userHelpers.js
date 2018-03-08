93345dc2a44a253a96f605dd95f3877474f59678
Nicholas Teal <nteal15@gmail.com>
2018-03-08 10:46:18 -0600

    (feat) adds rand planet id to new users

--- database/helpers/userHelpers.js
+++ database/helpers/userHelpers.js
@@ -1,8 +1,10 @@
 const { User } = require('../models/userModel');
 
+const randPlanet = () => Math.floor(Math.random() * 10);
+
 const addNewUser = (newUserObj) => {
-  // TODO: add planet
-  User.build(newUserObj)
+  const userObj = Object.assign({ planet_id: randPlanet() }, newUserObj);
+  User.build(userObj)
     .save()
     .catch((err) => { console.log(err); });
 };
