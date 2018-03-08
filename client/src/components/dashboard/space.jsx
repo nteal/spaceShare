457a7aa6850667c3359e6beeeb4da60b29f195b7
Emily Yu <emilyyu518@gmail.com>
2018-03-07 21:30:39 -0600

    (init) populates dash spaces, makes mini-headings responsive

--- client/src/components/dashboard/space.jsx
+++ client/src/components/dashboard/space.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Space = (props) => {
+  const { id, name, purpose } = props.space;
+  console.log('space', props.space);
+  const glyph = purpose === 'Live' ? (
+    <i className="material-icons sidebar-icon">home</i>
+  ) : (
+    <i className="material-icons">business</i>
+  );
   return (
-    <div>
-      {/* space */}
-    </div>
+    <Link to={{ pathname: '/common-area', state: { spaceId: id } }}>
+      {glyph}{name}
+    </Link>
   );
 };
 
--- client/src/components/dashboard/spaces.jsx
+++ client/src/components/dashboard/spaces.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import Space from './space.jsx';
+import MediaQuery from 'react-responsive';
 
-class Spaces extends React.Component {
-  render() {
-    return (
+const Spaces = (props) => {
+  const { spaces } = props;
+  console.log('spaces', spaces);
+
+  return (
+    <div className="content-box">
+      <MediaQuery minDeviceWidth={800}>
+        <div className="mini-heading-box-top">
+          <h5>Your Spaces</h5>
+        </div>
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={600}>
+        <div className="mini-heading-box-top-mobile">
+          <h5>Your Spaces</h5>
+        </div>
+      </MediaQuery>
       <div>
-        Spaces
-        <Space />
+        {spaces.map(space => (
+          <Space space={space} />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Spaces;
