ac5d5be93e3c0e07b2bcd81545ce89ff757f68dd
Nicholas Teal <nteal15@gmail.com>
2018-03-08 12:35:00 -0600

    (init) retrieves user by id, adds gender to user

--- database/helpers/userHelpers.js
+++ database/helpers/userHelpers.js
@@ -1,4 +1,5 @@
 const { User } = require('../models/userModel');
+const options = require('./optionHelpers');
 
 const randPlanet = () => Math.floor(Math.random() * 10);
 
@@ -7,6 +8,22 @@ const addNewUser = (newUserObj) => {
   User.build(userObj)
     .save()
     .catch((err) => { console.log(err); });
+};
+
+const getUserById = (userId) => {
+  const userObj = {};
+  return User.findById(userId)
+    .then((user) => {
+      Object.assign(userObj, user.dataValues);
+      return Promise.all([
+        options.getGenderById(user.gender_id),
+      ]);
+    })
+    .then(([gender]) => {
+      userObj.gender = gender.self_identification;
+      return userObj;
+    })
+    .catch(err => console.log(err));
 };
 
 exports.addNewUser = addNewUser;
