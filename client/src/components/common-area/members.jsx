f168edb87866d7eb051b14cb1990d212b6079df5
Emily Yu <emilyyu518@gmail.com>
2018-03-08 11:52:34 -0600

    (refactor) changes members to stateless component

--- client/src/components/common-area/members.jsx
+++ client/src/components/common-area/members.jsx
@@ -2,35 +2,28 @@ import React from 'react';
 import ArrowLeftBoldCircle from 'mdi-react/ArrowLeftBoldCircleIcon.js';
 import Member from './member.jsx';
 
-class Members extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleBack = this.handleBack.bind(this);
-  }
-  handleBack() {
-    this.props.history.goBack();
-  }
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="heading-box">
-            <h1>Members</h1>
-          </div>
+const Members = (props) => {
+  const handleBack = () => {
+    props.history.goBack();
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="heading-box">
+          <h1>Members</h1>
         </div>
-        <div className="row">
-          <button className="custom-btn" onClick={this.handleBack}>
-            <ArrowLeftBoldCircle className="mdi-btn" height={50} width={50} fill="#6F5BC0" />
-          </button>
-        </div>
-        <div className="row">
-          <Member />
-        </div>
-
       </div>
-    );
-  }
-}
+      <div className="row">
+        <button className="custom-btn" onClick={handleBack}>
+          <ArrowLeftBoldCircle className="mdi-btn" height={50} width={50} fill="#6F5BC0" />
+        </button>
+      </div>
+      <div className="row">
+        <Member />
+      </div>
+
+    </div>
+  );
+};
 
 export default Members;
