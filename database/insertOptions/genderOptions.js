79bcd9f4afaf4b6c39d3de9f62118d3190763054
Nicholas Teal <nteal15@gmail.com>
2018-03-07 19:30:11 -0600

    (fix) only populates genderOptions if empty

--- database/insertOptions/genderOptions.js
+++ database/insertOptions/genderOptions.js
@@ -1,8 +1,16 @@
 const { Gender } = require('../models/genderModel');
 
+const initialGenderOptions = [
+  { self_identification: 'male' },
+  { self_identification: 'female' },
+];
+
+const addGenders = () => (
+  Gender.bulkCreate(initialGenderOptions)
+);
+
 exports.fillGenderOptions = () => (
-  Gender.bulkCreate([
-    { self_identification: 'male' },
-    { self_identification: 'female' },
-  ])
+  Gender.findAll()
+    .then(genders => (genders.length ? console.log('genders already populated') : addGenders()))
+    .catch(err => console.log(err))
 );
