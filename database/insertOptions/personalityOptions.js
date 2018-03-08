1d2283cba350a9d5389445e263988adedbf00451
Nicholas Teal <nteal15@gmail.com>
2018-03-07 19:29:39 -0600

    (fix) only populates personalityOptions if empty

--- database/insertOptions/personalityOptions.js
+++ database/insertOptions/personalityOptions.js
@@ -1,8 +1,17 @@
 const { Personality } = require('../models/personalityModel');
 
+const personalityList = [
+  { type: 'introvert' },
+  { type: 'extrovert' },
+];
+
+const addPersonalities = () => (
+  Personality.bulkCreate(personalityList)
+);
+
 exports.fillPersonalityOptions = () => (
-  Personality.bulkCreate([
-    { type: 'introvert' },
-    { type: 'extrovert' },
-  ])
+  Personality.findAll()
+    // if already personalities, print them. else add them
+    .then(personalities => (personalities.length ? console.log('personalities already populated') : addPersonalities()))
+    .catch(err => console.log(err))
 );
