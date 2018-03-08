b45e3e9eee6eac938a83f5217346e7f93b2e05d5
jsmichot <jsmichot@gmail.com>
2018-03-07 19:33:49 -0600

    (init) displays each todo item with checkbox

--- client/src/components/common-area/todo-list-item.jsx
+++ client/src/components/common-area/todo-list-item.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const TodoListItem = (props) => {
   return (
     <div>
-      {/* todo-list-item */}
+      <input className="form-check-input" type="checkbox" id={props.todo} />
+      <label className="form-check-label" htmlFor={props.key}>{props.todo}
+      </label>
     </div>
   );
 };
