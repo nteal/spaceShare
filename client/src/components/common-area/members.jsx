88d6f2625bbe3782372595bb9912c0cb749c9646
Emily Yu <emilyyu518@gmail.com>
2018-03-08 11:34:10 -0600

    (feat) adds back button to members page

--- client/src/components/common-area/members.jsx
+++ client/src/components/common-area/members.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import ArrowLeftBoldCircle from 'mdi-react/ArrowLeftBoldCircleIcon.js';
 import Member from './member.jsx';
 
 class Members extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleBack = this.handleBack.bind(this);
+  }
+  handleBack() {
+    this.props.history.goBack();
+  }
   render() {
     return (
-      <div>
-        Members
-        <Member />
+      <div className="container">
+        <div className="row">
+          <div className="heading-box">
+            <h1>Members</h1>
+          </div>
+        </div>
+        <div className="row">
+          <button className="custom-btn" onClick={this.handleBack}>
+            <ArrowLeftBoldCircle className="mdi-btn" height={50} width={50} fill="#6F5BC0" />
+          </button>
+        </div>
+        <div className="row">
+          <Member />
+        </div>
+
       </div>
     );
   }
