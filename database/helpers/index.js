0bc627dcd2af098e3bde0898db54c37b02f830ee
Nicholas Teal <nteal15@gmail.com>
2018-03-09 11:46:07 -0600

    (init) exposes function to check for user in db

--- database/helpers/index.js
+++ database/helpers/index.js
@@ -2,8 +2,10 @@ const { addNewUser } = require('./userHelpers');
 const { getUserByFbId } = require('./userHelpers');
 const { getUserById } = require('./userHelpers');
 const { updateUser } = require('./userHelpers');
+const { userInDb } = require('./userHelpers');
 
 exports.addNewUser = addNewUser;
 exports.getUserByFbId = getUserByFbId;
 exports.getUserById = getUserById;
 exports.updateUser = updateUser;
+exports.userInDb = userInDb;
