3e978451647eb34dfc54ecda3e1a074ac3d27f43
Nicholas Teal <nteal15@gmail.com>
2018-03-09 15:23:37 -0600

    (init) retrieves gender options

--- database/helpers/index.js
+++ database/helpers/index.js
@@ -1,13 +1,19 @@
+// user helpers:
 const { addNewUser } = require('./userHelpers');
 const { getUserByFbId } = require('./userHelpers');
 const { getUserById } = require('./userHelpers');
 const { updateUser } = require('./userHelpers');
 const { userInDb } = require('./userHelpers');
+// options helpers:
 const { addGender } = require('./genderHelpers');
+const { getGenders } = require('./optionHelpers');
 
+// user exports:
 exports.addNewUser = addNewUser;
 exports.getUserByFbId = getUserByFbId;
 exports.getUserById = getUserById;
 exports.updateUser = updateUser;
 exports.userInDb = userInDb;
+// options exports:
 exports.addGender = addGender;
+exports.getGenders = getGenders;
--- database/helpers/optionHelpers.js
+++ database/helpers/optionHelpers.js
@@ -8,7 +8,12 @@ const getPlanetById = planetId => Planet.findByPrimary(planetId);
 const getPersonalityById = personalityId => Personality.findByPrimary(personalityId);
 const getSleepById = sleepId => Sleep.findByPrimary(sleepId);
 
+const getGenders = () => Gender.findAll()
+  .then(genderList => genderList.map(gender => gender.dataValues))
+  .catch(err => console.log(err));
+
 exports.getGenderById = getGenderById;
 exports.getPlanetById = getPlanetById;
 exports.getPersonalityById = getPersonalityById;
 exports.getSleepById = getSleepById;
+exports.getGenders = getGenders;
