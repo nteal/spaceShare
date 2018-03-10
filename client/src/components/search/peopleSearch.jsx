b971a8cd7a6c60c58d12cc9c37d8a415b83fe733
jsmichot <jsmichot@gmail.com>
2018-03-09 19:11:22 -0600

    (init)adds sleep & personality preferences to view

--- client/src/components/search/peopleSearch.jsx
+++ client/src/components/search/peopleSearch.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 
 class PeopleSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sleep: 'Early bird',
+      personality: 'Introvert',
+    };
+  }
   render() {
     return (
       <div>
-        {/* PeopleSearch */}
+        <div className="row">
+          <h1>Person Search</h1>
+        </div>
+        <div className="row">
+          <h5>Would you prefer a...</h5>
+        </div>
+        <div className="row">
+          <div className="col-2 form-check" onChange={this.handleInputChange}>
+            <input className="form-check-input" type="radio" id="day" name="sleep" value="Early bird" />
+            <label className="form-check-label" htmlFor="day">
+              Early bird
+            </label>
+          </div>
+          <div className="col-2 form-check" onChange={this.handleInputChange}>
+            <input className="form-check-input" type="radio" id="night" name="sleep" value="Early bird" />
+            <label className="form-check-label" htmlFor="night">
+              Night Owl
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-2 form-check" onChange={this.handleInputChange}>
+            <input className="form-check-input" type="radio" id="introvert" name="personality" value="Introvert" />
+            <label className="form-check-label" htmlFor="introvert">
+              Introvert
+            </label>
+          </div>
+          <div className="col-2 form-check" onChange={this.handleInputChange}>
+            <input className="form-check-input" type="radio" id="extrovert" name="personality" value="Extrovert" />
+            <label className="form-check-label" htmlFor="extrovert">
+              Extrovert
+            </label>
+          </div>
+        </div>
       </div>
     );
   }
