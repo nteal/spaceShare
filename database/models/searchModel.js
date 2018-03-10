09aa8ee457c8ca5cc25fffd014346356da836f0a
Nicholas Teal <nteal15@gmail.com>
2018-03-10 11:13:09 -0600

    (refactor) adds max, min age & fb_id to searchModel removes userId and GenderId

--- database/models/searchModel.js
+++ database/models/searchModel.js
@@ -18,17 +18,21 @@ const { User } = require('./userModel');
 const Search = db.define('search', {
   price_min: Sequelize.DECIMAL(12, 2),
   price_max: Sequelize.DECIMAL(12, 2),
+  age_min: Sequelize.INTEGER,
+  age_max: Sequelize.INTEGER,
   location_specificity: Sequelize.STRING,
   location_search: Sequelize.STRING,
   zip: Sequelize.STRING,
   city: Sequelize.STRING,
   state: Sequelize.STRING,
   include_people: Sequelize.BOOLEAN,
+  fb_id: Sequelize.STRING,
 });
 
 // add relationships for many:1 relationships
-User.hasMany(Search, { foreignKey: 'user_id' });
-Gender.hasMany(Search, { foreignKey: 'gender_id' });
+// sequelize doesn't support targetKeys for hasmany.............
+// User.hasMany(Search, { foreignKey: 'fb_id', targetKey: 'fb_id' });
+// Gender.hasMany(Search, { foreignKey: 'gender_id' });
 Sleep.hasMany(Search, { foreignKey: 'sleep_id' });
 Personality.hasMany(Search, { foreignKey: 'personality_id' });
 
