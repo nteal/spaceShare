fd099a04449310a9845c775d53f88a0c20f954b3
Emily Yu <emilyyu518@gmail.com>
2018-03-11 14:47:00 -0500

    (fix) gets rid of unneeded helpers

--- server/helpers.js
+++ server/helpers.js
@@ -15,22 +15,4 @@ const getS3image = (filename, callback) => {
   });
 };
 
-const postS3image = (file, filename, callback) => {
-  const options = {
-    service: 's3',
-    region: 'us-east-1',
-    method: 'POST',
-    path: filename,
-    body: { image: file },
-  };
-
-  request(aws4.sign(options), (response) => {
-    callback(response);
-  });
-};
-
-const getSignature = (callback) => {
-  
-};
-
 exports.getS3image = getS3image;
