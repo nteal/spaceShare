c591942f5781ce0039a16d83282cf40aa3724bda
Emily Yu <emilyyu518@gmail.com>
2018-03-11 00:08:50 -0600

    (feat) adds aws s3 helpers

--- server/helpers.js
+++ server/helpers.js
@@ -1,2 +1,36 @@
 const request = require('request');
+const aws4 = require('aws4');
 const dotenv = require('dotenv').config();
+
+
+const getS3image = (filename, callback) => {
+  const options = {
+    service: 's3',
+    region: 'us-east-1',
+    path: filename,
+  };
+
+  request(aws4.sign(options), (response) => {
+    callback(response);
+  });
+};
+
+const postS3image = (file, filename, callback) => {
+  const options = {
+    service: 's3',
+    region: 'us-east-1',
+    method: 'POST',
+    path: filename,
+    body: { image: file },
+  };
+
+  request(aws4.sign(options), (response) => {
+    callback(response);
+  });
+};
+
+const getSignature = (callback) => {
+  
+};
+
+exports.getS3image = getS3image;
