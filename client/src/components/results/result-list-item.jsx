78f8fb44a5d9a8942f9acfeb04e878367660928d
jsmichot <jsmichot@gmail.com>
2018-03-12 15:08:15 -0500

    (init) links to either profile or listing

--- client/src/components/results/result-list-item.jsx
+++ client/src/components/results/result-list-item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
 
 class ResultListItem extends React.Component {
   constructor(props) {
@@ -6,15 +7,17 @@ class ResultListItem extends React.Component {
     this.state = {};
   }
   render() {
-    const { image, name, financial, about, description } = this.props;
+    const { image, name, financial, about, description, id, link } = this.props;
     return (
       <li className="media">
         <img className="mr-3" src={image} alt="Generic placeholder image" />
-          <div className="media-body">
+        <div className="media-body">
+          <Link to={link}>
             <h4 className="mt-0 mb-1">{name}</h4>
-            <h5 className="mt-0 mb-1">{financial}</h5>
-            <h5 className="mt-0 mb-2">{about}</h5>
-            <p className="mt-0 mb-1">{description}</p>
+          </Link>
+          <h5 className="mt-0 mb-1">{financial}</h5>
+          <h5 className="mt-0 mb-2">{about}</h5>
+          <p className="mt-0 mb-1">{description}</p>
         </div>
       </li>
     );
