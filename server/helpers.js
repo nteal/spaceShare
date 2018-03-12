50ab1ab2916ab571c2f2f7c74017d2f1464c5918
Nicholas Teal <nteal15@gmail.com>
2018-03-12 14:59:30 -0500

    (init) adds initial geocoding functions

--- server/helpers.js
+++ server/helpers.js
@@ -15,4 +15,22 @@ const getS3image = (filename, callback) => {
   });
 };
 
+const geocode = (address) => {
+  return new Promise((resolve, reject) => {
+    request(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GEOCODE_KEY}`, (err, res, body) => {
+      err ? reject(err) : resolve(JSON.parse(body).results[0].geometry.location);
+    })
+  })
+};
+
+const reverseGeocode = (lat, long) => {
+  return new Promise((resolve, reject) => {
+    request(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.GEOCODE_KEY}`, (err, res, body) => {
+      err ? reject(err) : resolve(JSON.parse(body).results[0]); 
+    })
+  })
+}
+
 exports.getS3image = getS3image;
+exports.geocode = geocode;
+exports.reverseGeocode = reverseGeocode;
