1fb4f27519a372d902dd3f75f6c2263c79d0748c
Emily Yu <emilyyu518@gmail.com>
2018-03-11 23:19:29 -0500

    (fix) changes todo components to match real data, have key

--- client/src/components/common-area/todo-list-item.jsx
+++ client/src/components/common-area/todo-list-item.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const TodoListItem = (props) => {
+  const { id, content, completed } = props.todo;
   return (
     <div>
-      <input className="form-check-input" type="checkbox" id={props.todo} />
-      <label className="form-check-label" htmlFor={props.key}>{props.todo}
+      <input className="form-check-input" type="checkbox" id={id} />
+      <label className="form-check-label" htmlFor={id}>{content}
       </label>
     </div>
   );
