8aaf5e419c849ba2310f06001d10fef97fd2d09f
jsmichot <jsmichot@gmail.com>
2018-03-12 04:46:38 -0500

    (init) adds tabs to results component

--- client/src/components/results/index.jsx
+++ client/src/components/results/index.jsx
@@ -6,20 +6,40 @@ class Results extends React.Component {
     this.state = {};
   }
   render() {
-    const { heading } = this.props;
+    const { heading, people, places } = this.props;
     return (
       <div>
-        <div className="row">
-          <div className="col">
+        <div className="row mb-2 mt-2">
+          <div className="col-10">
             <h1>{heading}</h1>
           </div>
-          <div className="col">
-            <div className="row">
-              <button>New Search</button>
-            </div>
+          <div className="col-2">
+            <button>New Search</button>
           </div>
         </div>
-
+        <div className="row">
+          <div className="col">
+            <ul className="nav nav-tabs" role="tablist">
+              <li className="nav-item">
+                <a className="nav-link" id="peopletab" data-toggle="tab" href="#people" role="tab">People</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="placestab" data-toggle="tab" href="#places" role="tab">Places</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col">
+          <ul className="nav nav-tabs justify-content-end" role="tablist">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Past Searches</a>
+            </li>
+          </ul>
+          </div>
+        </div>
+        <div className="tab-content" id="myTabContent">
+          <div className="tab-pane fade show active" id="people" role="tabpanel">{people}</div>
+          <div className="tab-pane fade" id="places" role="tabpanel">{places}</div>
+        </div>
       </div>
     );
   }
