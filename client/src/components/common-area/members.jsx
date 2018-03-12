506b1e9817ba02715ca486f0a237e4fa1e9755bb
Emily Yu <emilyyu518@gmail.com>
2018-03-11 21:52:09 -0500

    (fix) makes back button more responsive and consistent with the one in listing

--- client/src/components/common-area/members.jsx
+++ client/src/components/common-area/members.jsx
@@ -9,35 +9,44 @@ const Members = (props) => {
     props.history.goBack();
   };
   return (
-    <div className="container">
-      <div className="row">
-        <MediaQuery minDeviceWidth={800}>
-          <div className="heading-box mt-neg">
-            <h1>Our Members</h1>
-          </div>
-        </MediaQuery>
-        <MediaQuery maxDeviceWidth={600}>
-          <div className="mobile-heading-box mt-neg">
-            <h2>Our Members</h2>
-          </div>
-        </MediaQuery>
-      </div>
-      <div className="row">
+    <div>
+      
+      <MediaQuery minDeviceWidth={800}>
         <button className="custom-btn" onClick={handleBack}>
           <ArrowLeftBoldCircle className="mdi-btn" height={50} width={50} fill="#6F5BC0" />
         </button>
-      </div>
-      <main>
-        <div className="row justify-content-around">
-          {members.map(member => (
-            member.fb_id === ownerId ? (
-              <Member member={member} key={member.id} owner />
-            ) : (
-              <Member member={member} key={member.id} />
-            )
-          ))}
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={799}>
+        <button className="custom-btn mobile-back-btn" onClick={handleBack}>
+          <ArrowLeftBoldCircle className="mdi-btn" height={75} width={75} fill="#6F5BC0" />
+        </button>
+      </MediaQuery>
+
+      <div className="container mt-neg-3">
+        <div className="row">
+          <MediaQuery minDeviceWidth={800}>
+            <div className="heading-box mt-neg">
+              <h1>Our Members</h1>
+            </div>
+          </MediaQuery>
+          <MediaQuery maxDeviceWidth={600}>
+            <div className="mobile-heading-box mt-neg-3">
+              <h2>Our Members</h2>
+            </div>
+          </MediaQuery>
         </div>
-      </main>
+        <main>
+          <div className="row pt-2 justify-content-around">
+            {members.map(member => (
+              member.fb_id === ownerId ? (
+                <Member member={member} key={member.id} owner />
+              ) : (
+                <Member member={member} key={member.id} />
+              )
+            ))}
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
