f3b9078ba1690f124589bd2e02324850430e25fc
jsmichot <jsmichot@gmail.com>
2018-03-12 07:09:19 -0500

    (init) sets state and passes props to Results comp

--- client/src/components/search-results/index.jsx
+++ client/src/components/search-results/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import Axios from 'axios';
+import Results from '../results/index.jsx';
 
 class SearchResults extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      people: [{
+        image_url: '',
+        name_first: 'no people were found for this search',
+        name_last: '',
+        profession: '',
+        sleep: '',
+        personality: '',
+      }],
+      places: [{
+        main_image: '',
+        name: 'There are no results for this search.',
+        cost: '',
+        neighborhood: '',
+        description: '',
+      }],
     };
   }
   componentDidMount() {
@@ -17,12 +32,24 @@ class SearchResults extends React.Component {
     //       search_id: localStorage.getItem('id_search'),
     //     }
     //   })
+    //     .then((response) => {
+    //       this.setState({
+    //         people: response.data.people,
+    //         places: response.data.places
+    //       });
+    //     })
+    //     .catch((error) => {console.log(error)});
     // }
   }
   render() {
+    const { people, places } = this.state;
     return (
       <div>
-        {/* search results */}
+        <Results
+          heading="Your Results"
+          people={people}
+          places={places} 
+        />
       </div>
     );
   }
