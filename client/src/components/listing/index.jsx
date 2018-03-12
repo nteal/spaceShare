a547c2dd695335a55504c2905d9241efb58f8d89
Emily Yu <emilyyu518@gmail.com>
2018-03-11 21:27:33 -0500

    (feat) makes page more responsive

--- client/src/components/listing/index.jsx
+++ client/src/components/listing/index.jsx
@@ -139,10 +139,17 @@ class Listing extends React.Component {
             />
           </div>
         </div>
-        <button className="custom-btn" onClick={this.handleBack}>
-          <ArrowLeftBoldCircle className="mdi-btn" height={50} width={50} fill="#6F5BC0" />
-        </button>
         <main>
+          <MediaQuery maxDeviceWidth={799}>
+            <button className="custom-btn mobile-back-btn" onClick={this.handleBack}>
+              <ArrowLeftBoldCircle className="mdi-btn" height={75} width={75} fill="#6F5BC0" />
+            </button>
+          </MediaQuery>
+          <MediaQuery minDeviceWidth={800}>
+            <button className="custom-btn" onClick={this.handleBack}>
+              <ArrowLeftBoldCircle className="mdi-btn" height={50} width={50} fill="#6F5BC0" />
+            </button>
+          </MediaQuery>
           <div className="container mt-neg-3">
             <div className="row">
               <MediaQuery minDeviceWidth={800}>
@@ -151,40 +158,65 @@ class Listing extends React.Component {
                 </div>
               </MediaQuery>
               <MediaQuery maxDeviceWidth={600}>
-                <div className="mobile-heading-box mt-neg">
+                <div className="mobile-heading-box mt-neg-3">
                   <h2>{name}</h2>
                 </div>
               </MediaQuery>
             </div>
             <div className="row pt-1">
               <div className="col">
-                <div className="d-flex flex-row align-items-center">
-                  {purposeGlyph}<h3 className="mb-0">{purpose}</h3>
-                </div>
-                <div className="d-flex flex-row align-items-center">
-                  <Link to={{ pathname: '/messages', state: { userId: owner_fb_id } }}>
-                    <Email className="mdi-btn ml-1 mr-2" height={20} width={20} fill="#6F5BC0" />
-                  </Link>
-                  <h5>
-                    {owner_name}
-                  </h5>
-                </div>
+                <MediaQuery minDeviceWidth={800}>
+                  <div className="d-flex flex-row align-items-center">
+                    {purposeGlyph}<h3 className="mb-0">{purpose}</h3>
+                  </div>
+                  <div className="d-flex flex-row align-items-center">
+                    <Link to={{ pathname: '/messages', state: { userId: owner_fb_id } }}>
+                      <Email className="mdi-btn ml-1 mr-2" height={20} width={20} fill="#6F5BC0" />
+                    </Link>
+                    <h5>
+                      {owner_name}
+                    </h5>
+                  </div>
+                </MediaQuery>
+                <MediaQuery maxDeviceWidth={600}>
+                  <div className="d-flex flex-row align-items-center">
+                    {purposeGlyph}<h4 className="mb-0">{purpose}</h4>
+                  </div>
+                  <div className="d-flex flex-row align-items-center">
+                    <Link to={{ pathname: '/messages', state: { userId: owner_fb_id } }}>
+                      <Email className="mdi-btn ml-1 mr-2" height={20} width={20} fill="#6F5BC0" />
+                    </Link>
+                    <h6>
+                      {owner_name}
+                    </h6>
+                  </div>
+                </MediaQuery>
               </div>
               <div className="col">
-                <div className="row justify-content-end">
-                  <h4 className="mb-0">${cost} / {timeline}</h4>
-                </div>
-                <div className="row justify-content-end">
-                  <h5>Space available for {capacity} {pronoun}</h5>
-                </div>
+                <MediaQuery minDeviceWidth={800}>
+                  <div className="row justify-content-end">
+                    <h4 className="mb-0">${cost} / {timeline}</h4>
+                  </div>
+                  <div className="row justify-content-end">
+                    <h5>Space available for {capacity} {pronoun}</h5>
+                  </div>
+                </MediaQuery>
+                <MediaQuery maxDeviceWidth={600}>
+                  <div className="row justify-content-end">
+                    <h5 className="mb-0">${cost} / {timeline}</h5>
+                  </div>
+                  <div className="row justify-content-end">
+                    <h6>Space available for {capacity} {pronoun}</h6>
+                  </div>
+                </MediaQuery>
               </div>
             </div>
             <div className="row pt-res">
-              <div className="col-12 col-sm-10 col-md-8 col-lg-8 d-flex flex-column pl-0 align-items-start">
+              <div className="col-12 col-sm-12 col-md-8 col-lg-8 d-flex flex-column pl-0 pr-0 pr-sm-0 pr-md-2 pr-lg-2 pr-xl-2">
                 <AboutSpace text={description} />
                 {locationDisplay}
               </div>
-              <div className="col-12 col-sm-10 col-md-4 col-lg-4 d-flex flex-column pr-0 align-items-end">
+              <div className="col-12 col-sm-12 col-md-4 col-lg-4 d-flex flex-column pr-0 pl-0 pl-sm-0 pl-md-2 pl-lg-2 pl-xl-2">
                 <Amenities pet={pet} smoking={smoking} amenities={amenities} />
               </div>
             </div>
