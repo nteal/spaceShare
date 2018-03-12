6491ffc04bc2aa73ccc3b8106400fcb585f34b77
Emily Yu <emilyyu518@gmail.com>
2018-03-11 19:59:08 -0500

    (init) adds amenities and amenity components

---
+++ client/src/components/listing/amenities.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import MediaQuery from 'react-responsive';
+import Amenity from './amenity.jsx';
+
+const Amenities = (props) => {
+  const { pet, smoking, amenities } = props;
+
+  return (
+    <div className="content-box">
+      <MediaQuery minDeviceWidth={800}>
+        <div className="mini-heading-box-top">
+          <h5>Amenities</h5>
+        </div>
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={600}>
+        <div className="mini-heading-box-top-mobile">
+          <h5>Amenities</h5>
+        </div>
+      </MediaQuery>
+      <div className="invisible-content-box">
+        <ul>
+          <li pb-1>
+            <div className="row">
+              <i className="material-icons md-sm sidebar-icon">pets</i>
+              <b>Pets: {pet}</b>
+            </div>
+          </li>
+          <li pb-1>
+            <div className="row">
+              <i className="material-icons md-sm sidebar-icon">smoking_rooms</i>
+              <b>Smoking: {smoking}</b>
+            </div>
+          </li>
+          {amenities.map(amenity => (
+            <Amenity amenity={amenity} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Amenities;
---
+++ client/src/components/listing/amenity.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Rocket from 'mdi-react/RocketIcon.js';
+
+const Amenity = (props) => {
+  const { amenity } = props;
+  return (
+    <li pb-1>
+      <div className="row">
+        <Rocket className="sidebar-icon" height={20} width={20} />
+        {amenity}
+      </div>
+    </li>
+  );
+};
+
+export default Amenity;
