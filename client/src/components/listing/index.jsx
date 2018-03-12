2a0ad64b1267145e2cabef6dc67272ad032491ea
Emily Yu <emilyyu518@gmail.com>
2018-03-11 19:27:06 -0500

    (init) imports other content components

--- client/src/components/listing/index.jsx
+++ client/src/components/listing/index.jsx
@@ -5,6 +5,10 @@ import { Link } from 'react-router-dom';
 import Pencil from 'mdi-react/PencilIcon.js';
 import ArrowLeftBoldCircle from 'mdi-react/ArrowLeftBoldCircleIcon.js';
 import Email from 'mdi-react/EmailIcon.js';
+import Location from './location.jsx';
+import AboutSpace from './aboutSpace.jsx';
+import Amenities from './amenities.jsx';
+import Gallery from './gallery.jsx';
 
 class Listing extends React.Component {
   constructor(props) {
@@ -33,6 +37,8 @@ class Listing extends React.Component {
           capacity,
           open,
           description,
+          pet,
+          smoking,
           amenities,
           gallery,
         } = response.data;
@@ -48,6 +54,8 @@ class Listing extends React.Component {
           capacity,
           open,
           description,
+          pet,
+          smoking,
           amenities,
           gallery,
         });
@@ -95,6 +103,8 @@ class Listing extends React.Component {
       timeline,
       capacity,
       description,
+      pet,
+      smoking,
       amenities,
       gallery,
     } = this.state;
@@ -106,6 +116,15 @@ class Listing extends React.Component {
     );
 
     const pronoun = capacity > 1 ? 'people' : 'person';
+
+    let locationDisplay;
+    if (open) {
+      const { neighborhood } = this.state;
+      locationDisplay = <Location open neighborhood={neighborhood} />;
+    } else {
+      const { street_address, city, state, zip } = this.state;
+      locationDisplay = <Location address={street_address} city={city} state={state} zip={zip} />;
+    }
 
     return (
       <div>
@@ -121,43 +140,57 @@ class Listing extends React.Component {
         <button className="custom-btn" onClick={this.handleBack}>
           <ArrowLeftBoldCircle className="mdi-btn" height={50} width={50} fill="#6F5BC0" />
         </button>
-        <div className="container mt-neg-3">
-          <div className="row">
-            <MediaQuery minDeviceWidth={800}>
-              <div className="heading-box mt-neg">
-                <h1>{name}</h1>
+        <main>
+          <div className="container mt-neg-3">
+            <div className="row">
+              <MediaQuery minDeviceWidth={800}>
+                <div className="heading-box mt-neg">
+                  <h1>{name}</h1>
+                </div>
+              </MediaQuery>
+              <MediaQuery maxDeviceWidth={600}>
+                <div className="mobile-heading-box mt-neg">
+                  <h2>{name}</h2>
+                </div>
+              </MediaQuery>
+            </div>
+            <div className="row pt-1">
+              <div className="col">
+                <div className="d-flex flex-row align-items-center">
+                  {purposeGlyph}<h3 className="mb-0">{purpose}</h3>
+                </div>
+                <div className="d-flex flex-row align-items-center">
+                  <Link to={{ pathname: '/messages', state: { userId: owner_fb_id } }}>
+                    <Email className="mdi-btn ml-1 mr-2" height={20} width={20} fill="#6F5BC0" />
+                  </Link>
+                  <h5>
+                    {owner_name}
+                  </h5>
+                </div>
               </div>
-            </MediaQuery>
-            <MediaQuery maxDeviceWidth={600}>
-              <div className="mobile-heading-box mt-neg">
-                <h2>{name}</h2>
-              </div>
-            </MediaQuery>
-          </div>
-          <div className="row pt-1">
-            <div className="col">
-              <div className="d-flex flex-row align-items-center">
-                {purposeGlyph}<h3 className="mb-0">{purpose}</h3>
-              </div>
-              <div className="d-flex flex-row align-items-center">
-                <Link to={{ pathname: '/messages', state: { userId: owner_fb_id } }}>
-                  <Email className="mdi-btn ml-1 mr-2" height={20} width={20} fill="#6F5BC0" />
-                </Link>
-                <h5>
-                  {owner_name}
-                </h5>
+              <div className="col">
+                <div className="row justify-content-end">
+                  <h4 className="mb-0">${cost} / {timeline}</h4>
+                </div>
+                <div className="row justify-content-end">
+                  <h5>Space available for {capacity} {pronoun}</h5>
+                </div>
               </div>
             </div>
-            <div className="col">
-              <div className="row justify-content-end">
-                <h4 className="mb-0">${cost} / {timeline}</h4>
+            <div className="row pt-res">
+              <div className="col-12 col-sm-10 col-md-8 col-lg-8">
+                <AboutSpace text={description} />
+                {locationDisplay}
               </div>
-              <div className="row justify-content-end">
-                <h5>Space available for {capacity} {pronoun}</h5>
+              <div className="col-12 col-sm-10 col-md-4 col-lg-4">
+                <Amenities pet={pet} smoking={smoking} amenities={amenities} />
               </div>
             </div>
+            <div className="row">
+              <Gallery images={gallery} />
+            </div>
           </div>
-        </div>
+        </main>
       </div>
     );
   }
