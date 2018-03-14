ae8351aab53d1f98cb40085c035a5960c5f48fca
Nicholas Teal <nteal15@gmail.com>
2018-03-13 19:05:42 -0500

    (fix) fixes usersWithSpaces

--- database/helpers/spaceMembersHelpers.js
+++ database/helpers/spaceMembersHelpers.js
@@ -34,7 +34,9 @@ const getSpaceMembers = spaceId => (
 const getSpacesByFbId = fbId => (
   getUserIdByFbId(fbId)
     .then(userId => UserSpace.findAll({ where: { userId: userId } }))
-    .then(userSpaces => Promise.map(userSpaces, userSpace => getDashboardInfoById(userSpace.spaceId)))
+    .then(userSpaces => Promise.map(userSpaces, userSpace => {
+      return getDashboardInfoById(userSpace.spaceId)
+    }))
     .catch(err => console.log(err))
 );
 
