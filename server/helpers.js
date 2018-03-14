6af4f6dafa39af920f8ed19abc1ef2ac73cb4cf7
Nicholas Teal <nteal15@gmail.com>
2018-03-14 11:52:18 -0500

    (refactor) refactors reverseGeo for places to only search for neighborhood

--- server/helpers.js
+++ server/helpers.js
@@ -23,55 +23,21 @@ const geocode = address => (
   })
 );
 
-reverseGeocodeSpace = (lat, long) => {
-  return new Promise((resolve, reject) => {
-    request(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.GEOCODE_KEY}`, (err, res, body) => {
-      if (err) {
-        reject(err);
-        return false;
-      }
-      const { results } = JSON.parse(body);
-    let streetNum = '';
-    let street = '';
-    const address = {
-      street_address: '',
-      neighborhood: '',
-      city: '',
-      state: '',
-      zip: '',
-    };
-    for (let i = 0; i < results; i += 1) {
-      for (let j = 0; j < results[i].length; j += 1) {
-        const addressComp = results[i][j];
-        // if everything populated, resolve and return!
-        // else try to fill them
-        if (address.street_address && address.city && address.state && address.zip && address.neighborhood) {
-          resolve(address);
-          return true;
-        } else if (!streetNum && addressComp.types.includes('street_number')) {
-          streetNum = addressComp.long_name;
-          address.street_address = streetNum && street && !address.street_address ? `${streetNum} ${street}` : false;
-        } else if (!street && addressComp.types.includes('route')) {
-          street = addressComp.short_name;
-          address.street_address = streetNum && street && !address.street_address ? `${streetNum} ${street}` : false;
-        } else if (!results.neighborhood && addressComp.types.includes('neighborhood')) {
-          results.neighborhood = addressComp.long_name;
-        } else if (!results.city && addressComp.types.includes('locality')) {
-          results.city = addressComp.long_name;
-        } else if (!results.state && addressComp.types.includes('administrative_area_level_1')) {
-          results.state = addressComp.short_name;
-        } else if (!results.zip && addressComp.types.includes('postal_code')) {
-          results.zip = addressComp.short_name;
-        }
+const reverseGeocodeSpace = (lat, lng, results) => {
+  const spaceLocation = { lat, lng, neighborhood: '' };
+  for (let i = 0; i < results.length; i += 1) {
+    for (let j = 0; j < results.length; j += 1) {
+      const addressComp = results[i][j];
+      if (addressComp.types.contains('neighborhood')) {
+        spaceLocation.neighborhood = addressComp.long_name;
+        return spaceLocation;
       }
     }
-    resolve(address);
-    return true;
-    });
-  });
+  }
 };
 
-reverseGeocodeSearch = (lat, lng) => {
+const reverseGeocodeSearch = (lat, lng) => {
+  // needs to save long/lat and city
   return new Promise((resolve, reject) => {
     request(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.GEOCODE_KEY}`, (err, res, body) => {
       if (err) {
@@ -95,11 +61,24 @@ reverseGeocodeSearch = (lat, lng) => {
 };
 
 const reverseGeocode = (purpose, lat, long) => {
-      if (purpose === 'search') {
-        return reverseGeocodeSearch(lat, long);
-      } else if (purpose === 'space') {
-        return reverseGeocodeSpace(lat, long);
+  return new Promise((resolve, reject) => {
+    request(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.GEOCODE_KEY}`, (err, res, body) => {
+      if (err) {
+        reject(err);
+        return false;
       }
+
+      const { results } = JSON.parse(body);
+    
+      if (purpose === 'search') {
+        resolve(reverseGeocodeSearch(lat, long, results));
+        return true;
+      } else if (purpose === 'space') {
+        resolve(reverseGeocodeSpace(lat, long, results));
+        return true;
+      }
+    });
+  });
 };
 
 exports.getS3image = getS3image;
