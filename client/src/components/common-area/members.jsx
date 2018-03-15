781526760385641b50fc32b3ec4a319ff104b5b6
Emily Yu <emilyyu518@gmail.com>
2018-03-14 22:23:29 -0500

    (feat) imports modal package to create a confirmation modal before allowing user to eject member

--- client/src/components/common-area/members.jsx
+++ client/src/components/common-area/members.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import MediaQuery from 'react-responsive';
 import PropTypes from 'prop-types';
+import ConfirmModal from 'react-bootstrap4-modal';
 import ArrowLeftBoldCircle from 'mdi-react/ArrowLeftBoldCircleIcon.js';
+import Rocket from 'mdi-react/RocketIcon.js';
 import Member from './member.jsx';
 
 class Members extends React.Component {
@@ -9,10 +11,15 @@ class Members extends React.Component {
     super(props);
     this.state = {
       newMember: '',
+      modalVisible: false,
+      ejectedMemberId: null,
     };
     this.handleBack = this.handleBack.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleAttemptDelete = this.handleAttemptDelete.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
   handleBack() {
     this.props.history.goBack();
@@ -21,14 +28,33 @@ class Members extends React.Component {
     const { value } = event.target;
     this.setState({ newMember: value });
   }
+  handleAttemptDelete(memberId) {
+    this.setState({
+      ejectedMemberId: memberId,
+      modalVisible: true,
+    });
+  }
   handleAdd() {
     const { addMember } = this.props;
     const { newMember } = this.state;
     addMember(newMember);
     this.setState({ newMember: '' });
   }
+  handleCancel() {
+    this.setState({
+      ejectedMemberId: null,
+      modalVisible: false,
+    });
+  }
+  handleDelete() {
+    const { ejectedMemberId } = this.state;
+    const { deleteMember } = this.props;
+    deleteMember(ejectedMemberId);
+    this.setState({ modalVisible: false });
+  }
   render() {
-    const { ownerId, members, deleteMember, isOwner } = this.props;
+    const { ownerId, members, isOwner } = this.props;
+    const { modalVisible } = this.state;
     return (
       <div>
         <MediaQuery minDeviceWidth={800}>
@@ -57,20 +83,50 @@ class Members extends React.Component {
           </div>
           <main>
             {isOwner && (
-              <div className="row pt-4">
-                <div className="input-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Add a member with their Facebook id"
-                    aria-label="Add a member with their Facebook id"
-                    onChange={this.handleChange}
-                    value={this.state.newMember}
-                  />
-                  <div className="input-group-append">
-                    <button className="btn btn-outline-secondary pb-0" type="button" onClick={this.handleAdd}>
-                      <i className="material-icons">add</i>
-                    </button>
+              <div>
+
+                <ConfirmModal visible={modalVisible} onOK={this.handleDelete} onCancel={this.handleCancel}>
+                  <div className="container pt-4 pl-4 pr-4 pb-0">
+                    <div className="content-box content-box-colorful">
+                      <div className="row justify-content-center">
+                        <div className="flex-column align-items-center">
+                          <div className="row justify-content-center">
+                            <Rocket className="modal-rocket" height={45} width={45} fill="#6F5BC0" />
+                          </div>
+                          <div className="row justify-content-center pt-2 pb-3">
+                            <p className="text-center">
+                              Are you sure you want to <br /> eject this user from your space?
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row justify-content-center modal-btns pb-2">
+                      <button className="btn btn-primary btn-sm pr-3 pb-0 mr-4" aria-label="confirm" onClick={this.handleDelete}>
+                        <i className="material-icons md-sm">check</i>
+                      </button>
+                      <button className="btn btn-primary btn-sm pr-3 pb-0" aria-label="cancel" onClick={this.handleCancel}>
+                        <i className="material-icons md-sm">close</i>
+                      </button>
+                    </div>
+                  </div>
+                </ConfirmModal>
+
+                <div className="row pt-4">
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Add a member with their Facebook id"
+                      aria-label="Add a member with their Facebook id"
+                      onChange={this.handleChange}
+                      value={this.state.newMember}
+                    />
+                    <div className="input-group-append">
+                      <button className="btn btn-outline-secondary pb-0" type="button" onClick={this.handleAdd}>
+                        <i className="material-icons">add</i>
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -78,9 +134,9 @@ class Members extends React.Component {
             <div className="row pt-2 justify-content-around">
               {members.map(member => (
                 member.fb_id === ownerId ? (
-                  <Member member={member} key={member.id} deleteMember={deleteMember} canDelete={isOwner} owner />
+                  <Member member={member} key={member.id} deleteMember={this.handleAttemptDelete} canDelete={isOwner} owner />
                 ) : (
-                  <Member member={member} key={member.id} deleteMember={deleteMember} canDelete={isOwner} />
+                  <Member member={member} key={member.id} deleteMember={this.handleAttemptDelete} canDelete={isOwner} />
                 )
               ))}
             </div>
