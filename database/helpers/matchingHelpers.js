19844b65ef3d61e6d152464408cd2c8f1d6f1064
Nicholas Teal <nteal15@gmail.com>
2018-03-14 20:41:39 -0500

    (lint) fixes linting errors in matchingHelpers

--- database/helpers/matchingHelpers.js
+++ database/helpers/matchingHelpers.js
@@ -3,16 +3,16 @@ const { getSearchesForMatching } = require('./searchHelpers');
 const { getSearchesByFbId } = require('./searchHelpers');
 const { Search } = require('../models/searchModel');
 
-const getAllMatches = searchId => {
-  return Search.findById(searchId)
-    .then(async search => {
+const getAllMatches = searchId => (
+  Search.findById(searchId)
+    .then(async (search) => {
       const matches = {};
       matches.places = await getSpacesForMatching(search.id);
       matches.people = search.include_people ? await getSearchesForMatching(search.id) : [];
       matches.searches = await getSearchesByFbId(search.fb_id);
       return matches;
     })
-    .catch(err => console.log(err));
-}
+    .catch(err => console.log(err))
+);
 
 exports.getAllMatches = getAllMatches;
