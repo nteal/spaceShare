d3e7d4388493bad0e4a1e877e3be68bcc5d41389
Nicholas Teal <nteal15@gmail.com>
2018-03-15 15:12:57 -0500

    (refactor) updates array of todos objects, takes space_id as param

--- database/helpers/todoHelpers.js
+++ database/helpers/todoHelpers.js
@@ -1,6 +1,6 @@
 const { Todo } = require('../models/todoModel');
 
-const getTodosBySpaceId = (spaceId) => (
+const getTodosBySpaceId = spaceId => (
   Todo.findAll({ where: { space_id: spaceId } })
     .then(todos => todos.map(todo => todo.dataValues))
     .catch(err => console.log(err))
@@ -20,14 +20,14 @@ const deleteTodo = todoId => (
     .then(todo => todo.destroy())
     .then(destroyed => destroyed.dataValues)
     .catch(err => console.log(err))
-)
+);
 
 // takes todo obj and either creates, deletes, or updates todo
 const updateTodo = (todoObj) => {
   if (todoObj.id && !todoObj.content) {
     // delete todo if given id but no content
     return deleteTodo(todoObj.id);
-  } else if (!todoObj.id && todoObj.content) {
+  } else if (!todoObj.id && todoObj.content && todoObj.space_id) {
     // create todo if given content but no id
     return addTodo(todoObj);
   } else if (todoObj.id && todoObj.content) {
@@ -36,11 +36,18 @@ const updateTodo = (todoObj) => {
       .then(todo => todo.update(todoObj))
       .then(updated => updated.dataValues)
       .catch(err => console.log(err));
-  } 
+  }
   // let server know that we didn't meet one of the above conditions
   console.log('please give valid data for updating a todo!');
   return false;
-}
+};
+
+const updateTodos = (spaceId, todos) => (
+  Promise.all(todos.map(todo => updateTodo(Object.assign({ space_id: spaceId }, todo))))
+    .then(updatedTodos => updatedTodos)
+    .catch(err => console.log(err))
+);
 
 exports.getTodosBySpaceId = getTodosBySpaceId;
 exports.updateTodo = updateTodo;
+exports.updateTodos = updateTodos;
