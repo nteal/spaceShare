941777abbb6ab58f0a5ef3ce82fb28dc198c2b5b
Nicholas Teal <nteal15@gmail.com>
2018-03-16 17:03:50 -0500

    (feat) adds helpers for matching spaces to searches

--- server/matchingHelp.js
+++ server/matchingHelp.js
@@ -1,4 +1,28 @@
-// make func to take current search, find all searches, then compare
-  // ensure that every object is within location, budget, and open
-  // sort every obj into arrays based on difference
-  // return object with different arrays based on differences
+const db = require('../database');
+const { getGeoDist } = require('./geocodeHelp');
+
+// match space helper for match
+// takes arr of spaces and current search
+  // else puts into nested array depending on differences
+
+const getDiffOfSpace = (search, place) => {
+  let diff = 0;
+  if (place.pet_id === search.pet_id) { diff += 1; }
+  if (place.smoking_id === search.smoking_id) { diff += 1; }
+  if (place.timeline_id === search.timeline_id) { diff += 1; }
+  return diff;
+};
+
+const matchSpaces = (currentSearch, allPlaces) => {
+  // remove places outside of search distance
+  const placesInDist = allPlaces.filter(space =>
+    getGeoDist({ lat: currentSearch.lat, lon: currentSearch.lng }, { lat: space.lat, lon: space.lng }) < currentSearch.distance);
+  // TODO: trim objs here?
+  // return arr or arrs ordered by difference between space and search
+  return placesInDist.reduce((orderedMatches, space) => {
+    const ind = getDiffOfSpace(currentSearch, space);
+    orderedMatches[ind] = orderedMatches[ind] ? orderedMatches[ind].concat(space) : [space];
+    return orderedMatches;
+  }, [[], [], [], []]);
+};
+
