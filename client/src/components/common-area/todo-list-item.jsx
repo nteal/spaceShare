8d995b0bf8846be69db4a63b31f126b4fc2ddad3
Emily Yu <emilyyu518@gmail.com>
2018-03-15 21:02:05 -0500

    (feat) adds more functionality to todo-list-items

--- client/src/components/common-area/todo-list-item.jsx
+++ client/src/components/common-area/todo-list-item.jsx
@@ -6,38 +6,91 @@ class TodoListItem extends React.Component {
     super(props);
     this.state = {
       editing: false,
-      isCompleted: false,
+      isComplete: false,
+      value: '',
     };
+    this.setTodo = this.setTodo.bind(this);
+    this.setComplete = this.setComplete.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
+    this.toggleEdit = this.toggleEdit.bind(this);
+  }
+  componentDidMount() {
+    const { complete } = this.props;
+    this.setComplete(complete);
+    this.setTodo();
+  }
+  setTodo() {
+    this.setState({ value: this.props.todo.content });
+  }
+  setComplete(isComplete) {
+    this.setState({ isComplete });
   }
   handleChange() {
-    this.props.toggleComplete(id);
+    const { todo, toggleComplete } = this.props;
+    const { id } = todo;
+    toggleComplete(id);
+    this.setState({ isComplete: !this.state.isComplete });
   }
   handleClick() {
-
+    this.setState({ editing: true });
+  }
+  handleTextChange(event) {
+    const { value } = event.target;
+    this.setState({ value });
+  }
+  handleEnter(event) {
+    const { updateTodo, todo } = this.props;
+    const { value } = this.state;
+    if (event.key === 'Enter') {
+      updateTodo(todo.id, value);
+      this.toggleEdit();
+    }
+  }
+  toggleEdit() {
+    this.setState({ editing: false });
   }
   render() {
-    const { isComplete } = this.state;
-    const { toggleComplete, complete, todo } = this.props;
-    const { id, content, completed } = todo;
-  
-    const display = complete ? (
-      <div>
-        <input className="form-check-input" type="checkbox" id={id} checked={isComplete} handleChange={this.handleChange} />
+    const { isComplete, editing, value } = this.state;
+    const { todo, complete } = this.props;
+    const { id, content } = todo;
+
+    let display;
+    if (editing) {
+      display = (
+        <input
+          type="text"
+          className="form-control"
+          aria-label="enter new todo or edit existing"
+          placeholder="Edit your todo"
+          value={value}
+          onChange={this.handleTextChange}
+          onKeyPress={this.handleEnter}
+          onBlur={this.toggleEdit}
+        />
+      );
+    } else if (complete) {
+      display = (
         <label className="form-check-label complete-todo" htmlFor={id}>
           {content}
         </label>
-      </div>
-    ) : (
-      <div>
-        <input className="form-check-input" type="checkbox" id={id} checked={isComplete} handleChange={this.handleChange} />
-        <label className="form-check-label incomplete-todo" htmlFor={id} onClick={this.handleClick} >
+      );
+    } else {
+      display = (
+        <label className="form-check-label incomplete-todo" htmlFor={id} onClick={this.handleClick}>
           {content}
         </label>
+      );
+    }
+
+    return (
+      <div className="pl-4">
+        <input className="form-check-input" type="checkbox" id={id} checked={isComplete} onChange={this.handleChange} />
+        {display}
       </div>
     );
-    return display;
   }
 }
 
@@ -47,6 +100,9 @@ TodoListItem.propType = {
     content: PropTypes.string,
     completed: PropTypes.bool,
   }),
+  complete: PropTypes.bool,
+  toggleComplete: PropTypes.func,
+  updateTodo: PropTypes.func,
 };
 
 TodoListItem.defaultProps = {
@@ -55,6 +111,9 @@ TodoListItem.defaultProps = {
     content: 'Add a todo!',
     completed: false,
   },
+  complete: false,
+  toggleComplete: null,
+  updateTodo: null,
 };
 
 export default TodoListItem;
