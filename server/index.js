e6e4f819c983b3fea77d12ba1df7d69bb5e13a04
jsmichot <jsmichot@gmail.com>
2018-03-17 16:15:36 -0500

    (init) requires offline-plugin/runtime at entry

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const app = require('./server.js');
 const dotenv = require('dotenv').config();
+require('offline-plugin/runtime').install();
 
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
-  console.log('listening on port ', port);
+  console.log('listening on port ', port); // eslint-disable-line
 });
