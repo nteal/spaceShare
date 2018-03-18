48d0fd159990649b8f606ebe79be5d20c26366a5
Emily Yu <emilyyu518@gmail.com>
2018-03-17 21:48:18 -0500

    (feat) moves logic for sending messages and displaying them into chat

--- client/src/components/common-area/chat.jsx
+++ client/src/components/common-area/chat.jsx
@@ -1,21 +1,101 @@
 import React from 'react';
 import MediaQuery from 'react-responsive';
+import PropTypes from 'prop-types';
+import ChatBubble from './chatBubble.jsx';
 
-const Chat = (props) => {
-  return (
-    <div className="content-box">
-      <MediaQuery minDeviceWidth={800}>
-        <div className="mini-heading-box-top">
-          <h5>Space Chat</h5>
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newMessage: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.sendMessageOnEnter = this.sendMessageOnEnter.bind(this);
+  }
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState({ newMessage: value });
+  }
+
+  sendMessage() {
+    const { newMessage } = this.state;
+    const { chat } = this.props;
+    // this.conversation
+    chat
+      .sendText(newMessage)
+      .then(() => {
+        this.setState({ newMessage: '' });
+      })
+      .catch(error => console.error('error sending message', error));
+  }
+
+  sendMessageOnEnter(event) {
+    const { newMessage } = this.state;
+    const { chat, incomingMessages } = this.props;
+    // this.conversation
+    if (event.key === 'Enter') {
+      chat
+        .sendText(newMessage)
+        .then(() => {
+          this.setState({ newMessage: '' });
+        })
+        .catch(error => console.error('error sending message', error));
+    }
+  }
+  render() {
+    const { newMessage } = this.state;
+    const { incomingMessages } = this.props;
+    return (
+      <div className="content-box">
+        <MediaQuery minDeviceWidth={800}>
+          <div className="mini-heading-box-top">
+            <h5>Space Chat</h5>
+          </div>
+        </MediaQuery>
+        <MediaQuery maxDeviceWidth={600}>
+          <div className="mini-heading-box-top-mobile">
+            <h5>Space Chat</h5>
+          </div>
+        </MediaQuery>
+        <div className="col">
+          <div className="row">
+            <div className="col">
+              {/* Messages go here */}
+              {incomingMessages.map(message => (
+                <ChatBubble message={message} />
+              ))}
+            </div>
+          </div>
+          <div className="row align-self-end pl-1 pr-1">
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                value={newMessage}
+                onChange={this.handleChange}
+                onKeyPress={this.sendMessageOnEnter}
+                aria-label="talk in your space chat"
+              />
+              <div className="input-group-append">
+                <button className="btn btn-outline-secondary pb-0" onClick={this.sendMessage} type="button">
+                  <i className="material-icons">add</i>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </MediaQuery>
-      <MediaQuery maxDeviceWidth={600}>
-        <div className="mini-heading-box-top-mobile">
-          <h5>Space Chat</h5>
-        </div>
-      </MediaQuery>
-    </div>
-  );
+      </div>
+    );
+  }
+}
+
+Chat.propTypes = {
+  incomingMessages: PropTypes.array,
+};
+
+Chat.defaultProps = {
+  incomingMessages: [],
 };
 
 export default Chat;
