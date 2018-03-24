e995c393253f1237c990cec6948dc814c08d328b
Emily Yu <emilyyu518@gmail.com>
2018-03-23 19:48:52 -0500

    (refactor) refactors header to be nav component

--- client/src/components/nav/header.jsx
+++ client/src/components/nav/header.jsx
@@ -4,19 +4,15 @@ import MediaQuery from 'react-responsive';
 import LogoutVariant from 'mdi-react/LogoutVariantIcon.js';
 
 const styles = {
-  root: {},
   header: {
     background: 'linear-gradient(45deg, #23037E, #BD00FF)',
-    color: 'white',
-    padding: '16px',
-    fontSize: '1.5em',
+    fontFamily: 'Raleway',
     maxHeight: '4rem',
   },
 };
 
 const Header = (props) => {
-  const rootStyle = styles.root;
-  const headerStyle = props.style ? props.style : styles.header;
+  const headerStyle = styles.header;
   const {
     hamburger,
     title,
@@ -25,45 +21,43 @@ const Header = (props) => {
   } = props;
 
   return (
-    <div className="container-fluid pl-0 pr-0" style={rootStyle}>
-      <div className="row" style={headerStyle}>
-        <div className="col-5 col-lg-6">
-          <div className="d-flex align-items-center">
-            {hamburger}
-            <MediaQuery minDeviceWidth={800}>
-              {title}
-              <span>
-                <Link to="/dashboard" className="header-link">
-                  My Dashboard
-                </Link>
-                {/* <span className="header-link">About</span>
-                <span className="header-link">Disclaimer</span> */}
-              </span>
-            </MediaQuery>
-          </div>
-        </div>
+    <div className="container-fluid pl-0 pr-0">
+      <nav className="navbar navbar-dark" style={headerStyle}>
+
         <MediaQuery maxDeviceWidth={799}>
-          <div className="col-2">
-            <div className="row justify-content-center">
-                {mobileTitle}
-            </div>
+          {hamburger}
+        </MediaQuery>
+
+        <MediaQuery minDeviceWidth={800}>
+          <div className="row align-items-baseline ml-0">
+            {title}
+            <span className="nav-item">
+              <Link to="/dashboard" className="header-link nav-link">
+                My Dashboard
+              </Link>
+            </span>
           </div>
         </MediaQuery>
-        <div className="col-5 col-lg-6">
-          <div className="row justify-content-end pr-2 pr-md-3 pr-lg-4 pr-xl-5">
-            <MediaQuery minDeviceWidth={800}>
-              <Link to="/" type="button" className="btn btn-outline-light" onClick={logout}>
-                Logout
-              </Link>
-            </MediaQuery>
-            <MediaQuery maxDeviceWidth={799}>
-              <Link to="/">
-                <LogoutVariant onClick={logout} className="mdi-btn" height={35} width={35} fill="#FFF" />
-              </Link>
-            </MediaQuery>
+
+        <MediaQuery maxDeviceWidth={799}>
+          <div className="row justify-content-center">
+              {mobileTitle}
           </div>
+        </MediaQuery>
+
+        <div className="row justify-content-end mr-0">
+          <MediaQuery minDeviceWidth={800}>
+            <Link to="/" type="button" className="btn btn-outline-light" onClick={logout}>
+              Logout
+            </Link>
+          </MediaQuery>
+          <MediaQuery maxDeviceWidth={799}>
+            <Link to="/">
+              <LogoutVariant onClick={logout} className="mdi-btn" height={35} width={35} fill="#FFF" />
+            </Link>
+          </MediaQuery>
         </div>
-      </div>
+      </nav>
       {props.children}
     </div>
   );
