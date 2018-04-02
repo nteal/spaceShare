409aef9e7555228c4cb063c393aaf5ffd310df0c
Emily Yu <emilyyu518@gmail.com>
2018-04-02 15:45:20 -0500

    (fix) adds privacy policy

---
+++ client/src/components/login/privacy.jsx
@@ -0,0 +1,215 @@
+import React from 'react';
+import MediaQuery from 'react-responsive';
+
+const PrivacyPolicy = props => (
+  <div className="container p-res">
+    <MediaQuery minDeviceWidth={800}>
+      <div className="heading-box descender">
+        <h1>Privacy Policy</h1>
+      </div>
+    </MediaQuery>
+    <MediaQuery maxDeviceWidth={799}>
+      <div className="mobile-heading-box descender">
+        <h2>Privacy Policy</h2>
+      </div>
+    </MediaQuery>
+    <div className="content-box mt-5">
+      <div className="invisible-content-box">
+        <div>
+          <p>
+            This privacy policy has been compiled to better serve those who are concerned with how their &quot;Personally Identifiable Information&quot;
+            (PII) is being used online. PII, as described in US privacy law and information security, is information that can be
+            used on its own or with other information to identify, contact, or locate a single person, or to identify an individual
+            in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or
+            otherwise handle your Personally Identifiable Information in accordance with our website.
+          </p>
+          <h5>What personal information do we collect from the people that visit our blog, website or app?</h5>
+          <p>
+            When ordering or registering on our site, as appropriate, you may be asked to enter your name, email address, mailing address,
+            phone number or other details to help you with your experience.
+          </p>
+          <h5>When do we collect information?</h5>
+          <p>We collect information from you when you register on our site, fill out a form or enter information on our site.</p>
+          <h5>How do we use your information? </h5>
+          <p> 
+            We may use the information we collect from you when you register, make a purchase, sign up for our newsletter, respond
+            to a survey or marketing communication, surf the website, or use certain other site features in the following ways:
+            <ul>
+              <li>
+                To personalize your experience and to allow us to deliver the type of content and product offerings in
+                which you are most interested.
+              </li>
+              <li>
+                To administer a contest, promotion, survey or other site feature.
+              </li>
+            </ul>
+          </p>
+          <h5>How do we protect your information?</h5>
+          <ul>
+            <li>We do not use vulnerability scanning and/or scanning to PCI standards.</li>
+            <li>We only provide articles and information. We never ask for credit card numbers.</li>
+            <li>We use regular Malware Scanning.</li>
+          </ul>
+          <p>
+            Your personal information is contained behind secured networks and is only accessible by a limited number of persons who
+            have special access rights to such systems, and are required to keep the information confidential. In addition, all sensitive/credit
+            information you supply is encrypted via Secure Socket Layer (SSL) technology.
+          </p>
+          <p>We implement a variety of security measures when a user enters, submits, or accesses their information to maintain the
+            safety of your personal information.
+          </p>
+          <p>All transactions are processed through a gateway provider and are not stored or processed on our servers.</p>
+          <h5>Do we use "cookies"?</h5>
+          <p>
+            Yes. Cookies are small files that a site or its service provider transfers to your computer&apos;s hard drive through your Web
+            browser (if you allow) that enables the site&apos;s or service provider&apos;s systems to recognize your browser and capture and
+            remember certain information. For instance, we use cookies to help us remember and process the items in your shopping
+            cart. They are also used to help us understand your preferences based on previous or current site activity, which enables
+            us to provide you with improved services. We also use cookies to help us compile aggregate data about site traffic and
+            site interaction so that we can offer better site experiences and tools in the future.
+          </p>
+          <p>
+            <strong>We use cookies to:</strong>
+            <ul>
+              <li>
+                Understand and save users&apos; preferences for future visits.
+              </li>
+            </ul>
+          </p>
+          <p>
+            You can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies.
+            You do this through your browser settings. Since browser is a little different, look at your browser&apos;s Help Menu to learn
+            the correct way to modify your cookies.
+          </p>
+          <p>
+            <strong>If users disable cookies in their browser:</strong>
+            <ul>
+              <li>
+                Some of the features that make your site experience more efficient may not function properly.
+              </li>
+            </ul>
+          </p>
+          <h5>Third-party disclosure</h5>
+          <p>
+            We do not sell, trade, or otherwise transfer to outside parties your Personally Identifiable Information unless we provide
+            users with advance notice. This does not include website hosting partners and other parties who assist us in operating
+            our website, conducting our business, or serving our users, so long as those parties agree to keep this information confidential.
+            We may also release information when its release is appropriate to comply with the law, enforce our site policies, or
+            protect ours or others&apos; rights, property, or safety.
+          </p>
+          <p> 
+            However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising,
+            or other uses. 
+          </p>
+          <h5>Third-party links</h5>
+          <p>
+            Occasionally, at our discretion, we may include or offer third-party products or services on our website. These third-party
+            sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content
+            and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback
+            about these sites.
+          </p>
+          <h5>Google</h5>
+          <p>
+            Google&apos;s advertising requirements can be summed up by <a href="https://support.google.com/adwordspolicy/answer/1316548?hl=en">Google&apos;s Advertising Principles</a>. They are put in place to provide
+            a positive experience for users.
+          </p>
+          <p>We have not enabled Google AdSense on our site but we may do so in the future.</p>
+          <h5>COPPA (Children Online Privacy Protection Act)</h5>
+          <p>
+            When it comes to the collection of personal information from children under the age of 13 years old, the Children&apos;s Online
+            Privacy Protection Act (COPPA) puts parents in control. The Federal Trade Commission, United States&apos; consumer protection
+            agency, enforces the COPPA Rule, which spells out what operators of websites and online services must do to protect children&apos;s
+            privacy and safety online.
+          </p>
+          <p>We do not specifically market to children under the age of 13 years old.</p>
+          <p>We do not let third-parties, including ad networks or plug-ins collect PII from children under 13.</p>
+          <h5>Fair Information Practices</h5>
+          <p>
+            The Fair Information Practices Principles form the backbone of privacy law in the United States and the concepts they include
+            have played a significant role in the development of data protection laws around the globe. Understanding the Fair Information
+            Practice Principles and how they should be implemented is critical to comply with the various privacy laws that protect
+            personal information.
+          </p>
+          <p>
+            <strong>
+              In order to be in line with Fair Information Practices we will take the following responsive action, should a data breach
+              occur:
+            </strong>
+            <ul>
+              <li>
+                We will notify you via email within 1 business day.
+              </li>
+            </ul>
+          </p>
+          <p>
+            We also agree to the Individual Redress Principle which requires that individuals have the right to legally pursue enforceable
+            rights against data collectors and processors who fail to adhere to the law. This principle requires not only that individuals
+            have enforceable rights against data users, but also that individuals have recourse to courts or government agencies
+            to investigate and/or prosecute non-compliance by data processors.
+          </p>
+          <h5>CAN SPAM Act</h5>
+          <p>The CAN-SPAM Act is a law that sets the rules for commercial email, establishes requirements for commercial messages, gives
+            recipients the right to have emails stopped from being sent to them, and spells out tough penalties for violations.
+          </p>
+          <p>
+            <strong>We collect your email address in order to:</strong>
+            <ul>
+              <li>
+                Send information, respond to inquiries, and/or other requests or questions.
+              </li>
+            </ul>
+          </p>
+          <p>
+            <strong>To be in accordance with CANSPAM, we agree to the following:</strong>
+            <ul>
+              <li>
+                Not use false or misleading subjects or email addresses.
+              </li>
+              <li>
+                Identify the message as an advertisement in some reasonable way.
+              </li>
+              <li>
+                Include the physical address of our business or site headquarters.
+              </li>
+              <li>
+                Monitor third-party email marketing services for compliance, if one is used.
+              </li>
+              <li>
+                Honor opt-out/unsubscribe requests quickly.
+              </li>
+              <li>
+                Allow users to unsubscribe by using the link at the bottom of each email.
+              </li>
+            </ul>
+          </p>
+          <p>
+            <strong>
+              If at any time you would like to unsubscribe from receiving future emails, you can email us at <a href="mailto:harvey@operationspark.org">harvey@operationspark.org</a>.
+            </strong>
+          </p>
+          <p>
+            Follow the instructions at the bottom of each email and we will promptly remove you from <strong>ALL</strong> correspondence.
+          </p> 
+        </div>
+        <h5>Contacting Us</h5>
+        <p>
+          If there are any questions regarding this privacy policy, you may contact us using the information below.
+          <br />
+          <br />
+          SpaceShare <br />
+          748 Camp Street <br />
+          Suite 2 <br />
+          New Orleans, Louisiana 70130 <br />
+          United States <br />
+          <a href="mailto:max@operationspark.org">max@operationspark.com</a> <br />
+        </p>
+        <br />
+        <p>
+          <i>Last Edited on March 23, 2018.</i>
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default PrivacyPolicy;
